8c3273f9bff9bc60fafa6291f50dd04abe416a4c
ArnaudMorks <arnaud.morks@gmail.com>
2024-03-12 13:33:43 +0100

    AM Test scene

--- Assets/Scripts/PlayerScripts/SC_Player.cs
+++ Assets/Scripts/PlayerScripts/SC_Player.cs
@@ -4,15 +4,35 @@ using UnityEngine;
 
 public class SC_Player : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] private Rigidbody myRigidbody;
+
+
+    [SerializeField] private float baseMoveSpeed = 5f;
+    [SerializeField] private float moveSpeed;
+    Vector3 movement;
+
+
     void Start()
     {
-        
+        myRigidbody = gameObject.GetComponent<Rigidbody>();
+        moveSpeed = baseMoveSpeed;      //de logica; "moveSpeed" kan veranderen en de "baseMovespeed" is de standaard snelheid waar de player weer terug naar kan veranderen
     }
 
-    // Update is called once per frame
+
     void Update()
     {
-        
+        movement.x = Input.GetAxisRaw("Horizontal");
+
+        //De movement gekoppeld aan de input manager van Horizontal en Vertical
+        movement.z = Input.GetAxisRaw("Vertical");
+
+        movement.Normalize();
+
+
+    }
+
+    void FixedUpdate()
+    {
+        myRigidbody.MovePosition(myRigidbody.position + movement * moveSpeed * Time.fixedDeltaTime);
     }
 }
