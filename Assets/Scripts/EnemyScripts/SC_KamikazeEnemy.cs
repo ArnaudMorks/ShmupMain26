0a53eae4122c21ed9195e062928015cf5dca14f1
Ruben Flemming <rfmw2007@gmail.com>
2024-03-14 11:12:32 +0100

    RF Updated the kamikaze enemy

--- Assets/Scripts/EnemyScripts/SC_KamikazeEnemy.cs
+++ Assets/Scripts/EnemyScripts/SC_KamikazeEnemy.cs
@@ -13,10 +13,9 @@ public class SC_KamikazeEnemy : SC_EnemyBase
 {
     private KamikazeState _kamikazeState;
     private Transform _playerTransform;
+    private Coroutine _kamikazeCoroutine = null;
 
-    [SerializeField]private float _searchDelay;
     [SerializeField] private float _enemyStopDrag;
-    [SerializeField] private float _searchRadius;
     [SerializeField] private float _playerLookTime;
     [SerializeField] private float _destroyTime;
 
@@ -25,7 +24,6 @@ public class SC_KamikazeEnemy : SC_EnemyBase
         _kamikazeState = KamikazeState.searching;
 
         _rigidBody.AddForce(transform.forward * _enemySpeed);
-        StartCoroutine(BeginKamikaze());
     }
 
     protected override void Update()
@@ -40,13 +38,7 @@ public class SC_KamikazeEnemy : SC_EnemyBase
 
     private IEnumerator BeginKamikaze()
     {
-        while(_kamikazeState == KamikazeState.searching)
-        {
-            // We dont really need to search in very quick succession, so we wait
-            yield return new WaitForSeconds(_searchDelay);
-
-            CheckForPlayer();
-        }
+        _kamikazeState = KamikazeState.lookingAtPlayer;
 
         // Stop the player (not instantly) when we're looking at the player
         _rigidBody.drag = _enemyStopDrag;
@@ -63,20 +55,16 @@ public class SC_KamikazeEnemy : SC_EnemyBase
         Destroy(gameObject, _destroyTime);
     }
 
-    private void CheckForPlayer()
+    private void OnTriggerEnter(Collider other)
     {
-        Collider[] hitColliders = Physics.OverlapSphere(transform.position, _searchRadius);
-        foreach(var hitCollider in hitColliders)
+        // Check if the collider we hit has the player component
+        if (other.TryGetComponent<SC_Player>(out _))
         {
-            // Check if the collider we hit has the player component
-            if(hitCollider.TryGetComponent<SC_Player>(out _))
-            {
-                // We need the transform of the player
-                _playerTransform =  hitCollider.transform;
+            // We need the transform of the player to look at it
+            _playerTransform = other.transform;
 
-                // Change the state so we can look at the player
-                _kamikazeState = KamikazeState.lookingAtPlayer;
-            }
+            // Start the kamikaze coroutine only once
+            _kamikazeCoroutine ??= StartCoroutine(BeginKamikaze());
         }
     }
 }
